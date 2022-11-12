96d906d3720ed55f4947400d8e734105b1e3f60c
Ana Clara Granato <anaclaragranato@gmail.com>
2022-11-12 19:42:41 +0100

    creating create and delete methods for room

---
+++ cours-dev-web-5-vuejs/demo_code/exercise_base/src/components/RoomsList.vue
@@ -0,0 +1,41 @@
+<template>
+    
+</template>
+
+<script>
+
+import axios from 'axios';
+import {API_HOST} from '../config';
+import WindowsListItem from './RoomsListItem.vue';
+import WindowForm from './RoomForm.vue';
+
+export default{
+    name: 'RoomsList',
+    components: {
+        RoomsListItem,
+        RoomForm,
+    },
+    data: function() {
+        return{
+            rooms: []
+        }
+    },
+    created: async function() { 
+        let response = await axios.get(`${API_HOST}/api/rooms`);
+        let rooms = response.data;
+        this.rooms = rooms;
+    },
+    methods: {
+        createRoom(newRoom){
+            this.rooms.push(newRoom);
+        },
+        async refresh(){
+            console.log("Refreshing");
+            let response = await axios.get(`${API_HOST}/api/rooms`);
+            let rooms = response.data;
+            this.rooms = rooms;
+        }
+    }
+}
+
+</script>
---
+++ cours-dev-web-5-vuejs/demo_code/exercise_base/src/components/RoomsListItem.vue
@@ -0,0 +1,33 @@
+<template>
+
+</template>
+
+<script>
+
+import axios from 'axios';
+import {API_HOST} from '../confg';
+ export default{
+     name: 'RoomsListItem',
+     props: ['room'],
+     data: function (){
+         return {isExpanded: false}
+     },
+     methods: {
+         toggleExpand() {
+             this.isExpanded = !this.isExpanded;
+         },
+         async switchRoom() {
+             let response = await axios.put(`${API_HOST}/api/rooms/${this.room.id}/switch`);
+             let updateRoom = response.data;
+             this.$emit('room-update', updateRoom);
+         },
+
+         async deleteWindow(){
+             let response = await axios.delete(`${API_HOST}/api/rooms/${this.window.id}`);
+             this.$emit('room-deleted');
+         }
+
+     }
+ }
+
+</script> 
